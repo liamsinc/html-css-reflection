dbb5bb007754d747fd8a0e1231ad6278c548a562
liamsinc <80267741+liamsinc@users.noreply.github.com>
2022-11-09 02:09:18 +0000

    Major upgrade and optimisation to app.js.

--- js/app.js
+++ js/app.js
@@ -1,54 +1,113 @@
-// -------------------------------------------------------------------------------
-// Side Menu
-// -------------------------------------------------------------------------------
-const enableScroll = {
-    overflow: 'auto',
-    height: 'auto' 
-}
+// ----------------------------------------------------------------
+// VARIABLES AND CONSTANTS
+// ----------------------------------------------------------------
+
+// JQuery selectors stored in constants:
+
+const $sideMenu = '.sidemenu';
+const $mainContent = '.main__content';
+const $menuButton = '.header__menu-btn';
+const $htmlBody = 'html, body';
+const $container = '.container';
+const $headerWrapper = '.header__wrapper';
+const $heroSection = '.hero-section';
+
+// JQuery helper constants:
+
+const $style = 'style';
+const $visible = ':visible';
+const $hidden = ':hidden';
+const $stickyClass = 'sticky__wrapper';
+
+// General constants: 
+
+const smallMenuWidth = 275;
+const largeMenuWidth = 350;
+const largeBreakpoint = 991;
+
+// CSS Objects
 
 const disableScroll = {
     overflow: 'hidden',
     height: '100%' 
-}
+};
+const stickyHero = { marginTop: '210px' };
+const stickyContainer = { marginLeft: '10px' };
 
-
-$('.header__menu-btn').on('click', function() {
-    if ($('.sidemenu').is(':hidden')) {
-        $('.sidemenu').show();
-        $('.container').css({marginLeft: '16px'})
-        $('html, body').css(disableScroll);
-    } else {
-        $('.sidemenu').hide();
-        $('.container').css({marginLeft: 'auto'})
-        $('html, body').css(enableScroll);
-    } 
-});
-
-// -------------------------------------------------------------------------------
-// Sticky Header
-// -------------------------------------------------------------------------------
+// Variable which holds the y position of the page:
 
 let oldScrollY = window.scrollY;
 
-const stickyHeroStyles = {
-    marginTop: '210px'
+// Variable used for storing the calculated main content width:
+
+let mainContentWidth;
+
+// ----------------------------------------------------------------
+// FUNCTIONS
+// ----------------------------------------------------------------
+
+// Function which calculates how wide the main content should be:
+
+function calculateMainContentWidth() {
+    let windowWidth = $(window).width();
+    if ($($sideMenu).is($visible)) {
+        if (windowWidth < largeBreakpoint) {
+            mainContentWidth = windowWidth - smallMenuWidth;
+            $($mainContent).css({width: mainContentWidth});
+        } else {
+            mainContentWidth = windowWidth - largeMenuWidth;
+            $($mainContent).css({width: mainContentWidth});
+        }    
+    } else {
+        $($mainContent).removeAttr($style);
+    }
 }
 
-const heroStyles = {
-    marginTop: '0px'
-}
+// Function which toggles the sticky header:
 
 function toggleStickyHeader() {
-    $('.header__wrapper').removeAttr('style');
-    if(window.scrollY === 0 || oldScrollY < window.scrollY){
-        $('.hero-section').css(heroStyles);
-        $('.header__wrapper').removeClass('sticky__wrapper');
+    $($headerWrapper).removeAttr($style);
+    let currentScrollY = window.scrollY;
+    if(currentScrollY === 0 || oldScrollY < currentScrollY){
+        $($heroSection).removeAttr($style);
+        $($headerWrapper).removeClass($stickyClass);
     } else {
-        $('.hero-section').css(stickyHeroStyles);
-        $('.header__wrapper').addClass('sticky__wrapper');
+        $($heroSection).css(stickyHero);
+        $($headerWrapper).addClass($stickyClass);
     }
-    oldScrollY = window.scrollY;
+    oldScrollY = currentScrollY;
 }
 
+// Function which toggles the side menu and calls a function:
+
+function toggleSideMenu() {
+    if ($($sideMenu).is($hidden)) {
+        $($sideMenu).show().scrollTop(0);
+        $($htmlBody).css(disableScroll);
+        $($container).css(stickyContainer);
+    } else {
+        $($sideMenu).hide();
+        $($htmlBody).removeAttr($style);
+        $($container).removeAttr($style);
+    }
+    calculateMainContentWidth();
+}
+
+// -------------------------------------------------------------------------------
+// SIDE MENU
+// -------------------------------------------------------------------------------
+
+$($menuButton).on('click', toggleSideMenu);
+
+// -------------------------------------------------------------------------------
+// STICKY HEADER
+// -------------------------------------------------------------------------------
+
 $(window).on('scroll', toggleStickyHeader);
+
+// ----------------------------------------------------------------
+// UTILITY
+// ----------------------------------------------------------------
+
+$(window).on('resize', calculateMainContentWidth);
 
