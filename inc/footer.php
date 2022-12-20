469a37bcfff951eb2f159e1ff0422979328c22c9
liamsinc <liam.sinclair0202@outlook.com>
2022-12-20 13:04:46 +0000

    Created footer and header .php. Renamed index.html to index.php. Included the footer and header and removed redundant markup from index.php

---
+++ inc/footer.php
@@ -0,0 +1,144 @@
+<footer class="footer-section">
+    <div class="container">
+        <div class="footer__row">
+            <div class="footer__services">
+                <div class="footer__service">
+                    <h4>About Netmatters</h4>
+                    <ul>
+                        <li>
+                            <a href="#">News</a>
+                        </li>
+                        <li>
+                            <a href="#">Our Careers</a>
+                        </li>
+                        <li>
+                            <a href="#">Our Team</a>
+                        </li>
+                        <li>
+                            <a href="#">Privacy Policy</a>
+                        </li>
+                        <li>
+                            <a href="#">Cookie Policy</a>
+                        </li>
+                        <li>
+                            <a href="#">Terms & Conditions</a>
+                        </li>
+                        <li>
+                            <a href="#">Environmental Policy</a>
+                        </li>
+                        <li>
+                            <a href="#">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="footer__service">
+                    <h4>Our Services</h4>
+                    <ul>
+                        <li>
+                            <a href="#">Bespoke Software</a>
+                        </li>
+                        <li>
+                            <a href="#">IT Support</a>
+                        </li>
+                        <li>
+                            <a href="#">Digital Marketing</a>
+                        </li>
+                        <li>
+                            <a href="#">Telecoms Services</a>
+                        </li>
+                        <li>
+                            <a href="#">Web Design</a>
+                        </li>
+                        <li>
+                            <a href="#">Cyber Security</a>
+                        </li>
+                        <li>
+                            <a href="#">Developer Training</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="footer__offices">
+                <div class="footer__office">
+                    <h4>Cambridge Office</h4>
+                    <ul>
+                        <li>Unit 1.31,</li>
+                        <li>St John's Innovation Centre,</li>
+                        <li>Cowley Road, Milton,</li>
+                        <li>Cambridge,</li>
+                        <li>CB4 0WS</li>
+                        <li>&nbsp;</li>
+                        <li>
+                            Tel: 
+                            <a href="tel:01223375772">01223 37 57 72</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="footer__office">
+                    <h4>Wymondham Office</h4>
+                    <ul>
+                        <li>Unit 15,</li>
+                        <li>Penfold Drive,</li>
+                        <li>Gateway 11 Business Park,</li>
+                        <li>Wymondham, Norfolk,</li>
+                        <li>NR18 0WZ</li>
+                        <li>&nbsp;</li>
+                        <li>
+                            Tel: 
+                            <a href="tel:01603704020">01603 70 40 20</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="footer__office">
+                    <h4>Great Yarmouth Office</h4>
+                    <ul>
+                        <li>Suite F23,</li>
+                        <li>Beacon Innovation Centre,</li>
+                        <li>Beacon Park, Gorleston,</li>
+                        <li>Great Yarmouth, Norfolk,</li>
+                        <li>NR31 7RA</li>
+                        <li>&nbsp;</li>
+                        <li>
+                            Tel: 
+                            <a href="tel:01493603204">01493 60 32 04</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="footer__bottom">
+            <div class="footer__license">
+            &copy; Copyright Netmatters 2022.&nbsp; 
+            <br class="visible-xs">All rights reserved.&nbsp;
+            <span>&nbsp;-&nbsp;&nbsp;</span>
+            <ul>
+                    <li>
+                        <a href="#">Sitemap</a> 
+                    </li>
+            </ul>
+            </div>
+            <div class="footer__social">
+                <a class="social__link" href="#">
+                    <div class="social__icon__wrapper siw-1">
+                        <i class="fa-brands fa-facebook-f social__icon"></i>
+                    </div>
+                </a>
+                <a class="social__link" href="#">
+                    <div class="social__icon__wrapper siw-2">
+                        <i class="fa-brands fa-twitter social__icon"></i>
+                    </div>
+                </a>
+                <a class="social__link" href="#">
+                    <div class="social__icon__wrapper siw-3">
+                        <i class="fa-brands fa-instagram social__icon"></i>
+                    </div>
+                </a>
+                <a class="social__link" href="#">
+                    <div class="social__icon__wrapper siw-4">
+                        <i class="fa-brands fa-linkedin-in social__icon"></i>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ inc/header.php
@@ -0,0 +1,149 @@
+<div class="header__wrapper">
+    <header class="header">
+        <div class="container">
+            <div class="header__top-row">
+                <a class="header__logo-link" href="#">
+                    <img src="img/logo.png" alt="Netmatters">
+                </a>
+                <button class="header__support-btn" type="button">
+                    <i class="fa-solid fa-computer-mouse"></i>
+                    Support
+                </button>
+                <button class="header__contact-btn" type="button">
+                    <i class="fa-regular fa-paper-plane"></i>
+                    Contact
+                </button>
+                <input class="header__search-sm" type="search" placeholder="Search...">
+                <button class="header__search-sm-btn" type="submit">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </button>
+                <a class="header__phone-link" href="#">
+                    <i class="fa-solid fa-phone-volume"></i>
+                </a>
+                <button class="header__menu-btn hamburger hamburger--collapse" type="button">
+                    <span class="hamburger-box">
+                        <span class="hamburger-inner"></span>
+                    </span>
+                </button>
+            </div>
+            <div class="header__bottom-row">
+                <a class="header__search-lg-link" href="#">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </a>
+                
+                <input class="header__search-lg" type="search" placeholder="Search...">
+            </div>   
+        </div>
+    </header>
+
+    <nav class="navigation">
+        <div class="container">
+            <div class="nav__wrapper">
+                <a class="nav__item nav__item-1" href="#">
+                    <i class="fa-solid fa-table-cells"></i>
+                    <h2>Bespoke</h2>
+                    <p>Software</p>
+                    <div class="nav__dropdown nav__dropdown-1">
+                        <ul>
+                            <li>Workflow</li>
+                            <li>Automation</li>
+                            <li>Integration</li>
+                            <li>Database</li>
+                            <li>Sharepoint</li>
+                            <li>Management</li>
+                            <li>Navision</li>
+                        </ul>
+                    </div>
+                </a>
+                <a class="nav__item nav__item-2" href="#">
+                    <i class="fa-solid fa-desktop"></i>
+                    <h2>IT</h2>
+                    <p>Support</p>
+                    <div class="nav__dropdown nav__dropdown-2">
+                        <ul>
+                            <li>Managed IT</li>
+                            <li>Business IT</li>
+                            <li>Office 365</li>
+                            <li>Consultancy</li>
+                            <li>Cloud Provider</li>
+                            <li>Data Backup</li>
+                        </ul>
+                    </div>
+                </a>
+                <a class="nav__item nav__item-3" href="#">
+                    <i class="fa-solid fa-chart-simple"></i>
+                    <h2>Digital</h2>
+                    <p>Marketing</p>
+                    <div class="nav__dropdown nav__dropdown-3">
+                        <ul>
+                            <li>Search (SEO)</li>
+                            <li>Paid (PPC)</li>
+                            <li>Conversion (CRO)</li>
+                            <li>Email</li>
+                            <li>Social Media</li>
+                            <li>Content</li>
+                        </ul>
+                    </div> 
+                </a>
+                <a class="nav__item nav__item-4" href="#"> 
+                    <i class="fa-solid fa-phone-volume"></i>
+                    <h2>Telecoms</h2>
+                    <p>Services</p>
+                    <div class="nav__dropdown nav__dropdown-4">
+                        <ul>
+                            <li>Business Mobile</li>
+                            <li>Hosted VoIP</li>
+                            <li>Business VoIP</li>
+                            <li>Business Broadband</li>
+                            <li>Leased Line</li>
+                            <li>3CX Systems</li>
+                        </ul>
+                    </div>
+                </a>
+                <a class="nav__item nav__item-5" href="#">  
+                    <i class="fa-solid fa-code"></i>
+                    <h2>Web</h2>
+                    <p>Design</p>
+                    <div class="nav__dropdown nav__dropdown-5">
+                        <ul>
+                            <li>Stylish Websites</li>
+                            <li>eCommerce Stores</li>
+                            <li>Branding</li>
+                            <li>Apps</li>
+                            <li>Web Hosting</li>
+                            <li>Pay Monthly Websites</li>
+                        </ul>
+                    </div> 
+                </a>
+                <a class="nav__item nav__item-6" href="#"> 
+                    <i class="fa-solid fa-shield-halved"></i>
+                    <h2>Cyber</h2>
+                    <p>Security</p>
+                    <div class="nav__dropdown nav__dropdown-6">
+                        <ul>
+                            <li>Assessment</li>
+                            <li>Management</li>
+                            <li>Penetration Testing</li>
+                            <li>Cyber Essentials</li>
+                            <li>PCI/DSS</li>
+                            <li>Hacker Prevention</li>
+                        </ul>
+                    </div>
+                </a>
+                <a class="nav__item nav__item-7" href="#"> 
+                    <i class="fa-solid fa-graduation-cap"></i>
+                    <h2>Developer</h2>
+                    <p>Course</p>
+                    <div class="nav__dropdown nav__dropdown-7">
+                        <ul>
+                            <li>Train For A Career In Tech</li>
+                            <li>Skills Bootcamp</li>
+                            <li>Scion Scheme Frequently Asked Questions</li>
+                            <li>Scion Collaborators</li>
+                        </ul>
+                    </div>  
+                </a>
+            </div>
+        </div>
+    </nav>
+</div>
