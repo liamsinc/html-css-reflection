d46068e677c4228bcf8fda07d596658c5f6dd250
liamsinc <liam.sinclair0202@outlook.com>
2023-01-03 09:37:39 +0000

    Test commit

--- inc/DatabaseModel.php
+++ inc/DatabaseModel.php
@@ -1,33 +1,51 @@
 <?php
-class DatabaseModel
-{
-    protected $server;
-    protected $database;
-    protected $username;
-    protected $password;
 
-    public function __construct($host, $db, $user, $pw = "") {
-        $this->server = $host;
-        $this->database = $db;
-        $this->username = $user;
-        $this->password = $pw;
-    }
+// echo phpinfo();
 
-    public function connect() {
-        try {
-            $connection = new PDO("mysql:host=$this->server;dbname=$this->database", $this->username, $this->password);
-            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Connection succeeded";
-        } catch (PDOException $e) {
-            echo "Connection failed: " . $e->getMessage();
-        }
-    }
+// class DatabaseModel
+// {
+//     protected $server;
+//     protected $database;
+//     protected $username;
+//     protected $password;
 
+//     public function __construct($host, $db, $user, $pw = "") {
+//         $this->server = $host;
+//         $this->database = $db;
+//         $this->username = $user;
+//         $this->password = $pw;
+//     }
+
+//     public function connect() {
+//         try {
+//             $connection = new PDO("mysql:host=$this->server;dbname=$this->database", $this->username, $this->password);
+//             $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//             echo "Connection succeeded";
+//         } catch (PDOException $e) {
+//             echo "Connection failed: " . $e->getMessage();
+//         }
+//     }
+
+// }
+
+// $db = new DatabaseModel('localhost', 'netmatters', 'root');
+
+// $db->connect();
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
 
-$db = new DatabaseModel('localhost/127.0.0.1', 'netmatters', 'root');
-
-$db->connect();
 
 
 ?>
