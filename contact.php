d3ee4dbe4ca6fb8d21d97416a87367e86c9fc571
liamsinc <liam.sinclair0202@outlook.com>
2023-01-03 15:45:58 +0000

    fixed database issue

--- contact.php
+++ contact.php
@@ -16,6 +16,24 @@
         <div class="overlay"></div>
         <div class="main__content">
             <?php require "inc/header.php"; ?>
+            <?php
+
+            $servername = "127.0.0.1";
+            $username = "root";
+            $password = "";
+
+            try {
+                echo "Hello\n";
+                $conn = new PDO("mysql:host=$servername;dbname=netmatters", $username, $password);
+                // set the PDO error mode to exception
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                echo "Connected successfully";
+            } catch(PDOException $e) {
+                echo "Connection failed: " . $e->getMessage();
+            }
+
+            
+            ?>
             
             <section class="breadcrumb__section">
                 <div class="navspan">
--- inc/DatabaseModel.php
+++ inc/DatabaseModel.php
@@ -33,12 +33,13 @@
 // $db->connect();
 
 
-$servername = "localhost";
+$servername = "127.0.0.1";
 $username = "root";
 $password = "";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    echo "Hello\n";
+    $conn = new PDO("mysql:host=$servername;dbname=netmatters", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
